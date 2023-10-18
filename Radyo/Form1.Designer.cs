47cadd3eae1be21ca1b8b7fe0905353ee15493bf
Emre İŞLER <147929551+Emre-Isler@users.noreply.github.com>
2023-10-18 21:59:49 +0200

    Add files via upload

---
+++ Radyo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Radyo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.cbRadyo = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-1, 98);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(221, 113);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // cbRadyo
+            // 
+            this.cbRadyo.BackColor = System.Drawing.Color.Bisque;
+            this.cbRadyo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbRadyo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.cbRadyo.FormattingEnabled = true;
+            this.cbRadyo.Items.AddRange(new object[] {
+            "NTV Radyo",
+            "90\'lar",
+            "Bucak FM",
+            "Slow Türk"});
+            this.cbRadyo.Location = new System.Drawing.Point(39, 231);
+            this.cbRadyo.Name = "cbRadyo";
+            this.cbRadyo.Size = new System.Drawing.Size(147, 26);
+            this.cbRadyo.TabIndex = 1;
+            this.cbRadyo.Text = "Radyo Seç";
+            this.cbRadyo.SelectedIndexChanged += new System.EventHandler(this.cbRadyo_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.BackgroundImage = global::Radyo.Properties.Resources.sdle;
+            this.ClientSize = new System.Drawing.Size(220, 273);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbRadyo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Radyo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ComboBox cbRadyo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Radyo/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Radyo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void radiocal()
+        {
+            SoundPlayer ses = new SoundPlayer();
+            string dizin = Application.StartupPath + "\\radio.wav";
+            ses.SoundLocation = dizin;
+            ses.Play();
+        }
+         private static void radiokpt()
+        {
+            SoundPlayer ses = new SoundPlayer();
+            string dizin = Application.StartupPath + "\\radio.wav";
+            ses.SoundLocation = dizin;
+            ses.Play();
+            ses.Stop();
+        }
+        private void cbRadyo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            radiocal();
+            timer1.Start();
+            switch (cbRadyo.Text)
+            {
+                case "NTV Radyo":
+                    axWindowsMediaPlayer1.URL = ("mms://ntvrdwmp.radyotvonline.com:80");
+                    break;
+                case "90'lar":
+                    axWindowsMediaPlayer1.URL = ("mms://37.247.98.8/stream/166/");
+                    break;
+                case "Bucak FM":
+                    axWindowsMediaPlayer1.URL = ("mms://95.173.184.20:9588/");
+                    break;
+                case "Slow Türk":
+                    axWindowsMediaPlayer1.URL = ("mms://radyo.duhnet.tv/ak_dtvh_slowturk/");
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbRadyo.Text = "NTV Radyo";
+            timer1.Start();
+            int zmn = 0;
+            radiocal();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zmn++;
+            if (zmn==5)
+            {
+                radiokpt();
+                timer1.Stop();
+                zmn = 0;
+            }
+        }
+
+
+        public int zmn { get; set; }
+    }
+}
